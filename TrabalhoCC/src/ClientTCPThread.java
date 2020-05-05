b3f470d79628d979de664e1d623b2f000ad68d17
Rui Mendes <ruifilipe29mendes@gmail.com>
2020-05-05 11:47:42 +0100

    Add class

---
+++ TrabalhoCC/src/ClientTCPThread.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ClientTCPThread implements Runnable{
+    private String ipAnon;
+    private int portAnon;
+    private String line;
+    private int session;
+    public ClientTCPThread(String line, String ipAnon, int portAnon) {
+        this.ipAnon = ipAnon;
+        this.portAnon = portAnon;
+        this.line=line;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Socket s = new Socket(ipAnon, portAnon);
+            System.out.println("Ligação Estabelecida");
+            PrintWriter out = new PrintWriter(System.out);
+            BufferedReader inSocket = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter outSocket = new PrintWriter(s.getOutputStream());
+            out.println("Entrou aqui!");
+            out.flush();
+            out.println("Teste - " + line);
+            out.flush();
+            outSocket.println(line);
+            outSocket.flush();
+            String response = inSocket.readLine();
+            out.println("RESPOSTA: "+response);
+            out.flush();
+            s.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
